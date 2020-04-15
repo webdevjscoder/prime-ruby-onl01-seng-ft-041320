36f60d17020e18f266954f4bb54a37e8449daf41
Juan Cobian <cobianjuan19@gmail.com>
2020-04-15 23:34:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 
 def prime?(number)
-  (2..(number - 1)).each do |n|
-    return false if num % n == 0
+  (2..(number - 1)).each do |num|
+    return false if number % num == 0
   end
   true
 end 
