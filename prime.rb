5c8ed38399302c2f04bd046f1162f3a56090ddc3
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 00:14:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  num = 2
+  if number > 1
+    range = (num..number-1).to_a
+    range.none? do |num_to_test|
+    number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
